fab6ba60f2badb73121b89892b46913686890647
Panteleev <dev@gleb.pw>
2020-03-14 11:53:41 +0300

    v0.0.5

--- coin_test.go
+++ coin_test.go
@@ -5,6 +5,6 @@ import "testing"
 func TestHello(t *testing.T) {
 	want := "it is ok"
 	if got := Coin(); got != want {
-		t.Errorf("Hello() = %q, want %q", got, want)
+		t.Errorf("Hello() = %q, want %q?", got, want)
 	}
 }
